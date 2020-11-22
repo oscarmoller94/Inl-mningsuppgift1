33e56f857b650aee1876b8cb021687c2892bb48b
oscarmoller94@outlook.com <oscarmoller94@outlook.com>
2020-11-22 18:50:23 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inlämningsuppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("Enter first operator:");
+            char.TryParse(Console.ReadLine(), out char firstOperator);
+            Console.WriteLine("Enter the second operator");
+            char.TryParse(Console.ReadLine(), out char secondOperator);
+            Console.WriteLine("Enter the third operator");
+            char.TryParse(Console.ReadLine(), out char thirdOperator);
+
+
+        }
+    }
+}
